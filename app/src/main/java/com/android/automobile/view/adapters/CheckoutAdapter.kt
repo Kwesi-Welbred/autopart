67b627ab54a66683694e5f2b51c7f93aaf48c876
Peacemaker <peacemaker.otoo@amalitech.org>
2022-07-12 21:50:44 +0000

    payment and checkout

---
+++ app/src/main/java/com/android/automobile/view/adapters/CheckoutAdapter.kt
@@ -0,0 +1,58 @@
+package com.android.automobile.view.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.android.automobile.databinding.CartListBinding
+import com.android.automobile.databinding.CheckoutListBinding
+import com.android.automobile.model.Cart
+import com.bumptech.glide.Glide
+
+class CheckoutAdapter(private val impl: CartImpl) :
+    ListAdapter<Cart, CheckoutAdapter.RecyclerViewHolder>(ListComparator()) {
+    //bind the recycler list items
+    inner class RecyclerViewHolder(val binding: CheckoutListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
+        fun bind(list: Cart?) {
+            binding.cartImage.load(list?.imgUrl)
+            binding.cartName.text = list?.brandName
+            binding.cartPrice.text = list?.price
+        }
+    }
+
+    //inflate the List
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+        return RecyclerViewHolder(
+            CheckoutListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    //bind the model list the recycler list
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        val getItemPosition = getItem(position)
+        holder.bind(getItemPosition)
+        Glide.with(holder.itemView.context).load(getItemPosition.imgUrl)
+            .into(holder.binding.cartImage)
+    }
+
+    class ListComparator : DiffUtil.ItemCallback<Cart>() {
+        override fun areItemsTheSame(oldItem: Cart, newItem: Cart): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Cart, newItem: Cart): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+    interface CartImpl {
+        fun onItemDeleteListener(car: Cart)
+    }
+
+}
--- app/src/main/java/com/android/automobile/view/fragments/PaymentFragment.kt
+++ app/src/main/java/com/android/automobile/view/fragments/PaymentFragment.kt
@@ -1,20 +1,43 @@
 package com.android.automobile.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RadioButton
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.automobile.R
 import com.android.automobile.databinding.FragmentPaymentBinding
+import com.android.automobile.di.AutoMobileApp
+import com.android.automobile.model.Cart
+import com.android.automobile.view.adapters.CartAdapter
 import com.android.automobile.view.util.navById
+import com.android.automobile.viewmodel.factories.ProductViewModelFactory
+import com.android.automobile.viewmodel.products.ProductViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 
-class PaymentFragment : Fragment() {
+class PaymentFragment : Fragment(), CartAdapter.CartImpl {
     lateinit var binding: FragmentPaymentBinding
+
+     private val recyclerAdapter: CartAdapter by lazy {
+            CartAdapter(this)
+        }
+
+     private val productViewmodel: ProductViewModel by viewModels {
+        ProductViewModelFactory((activity?.application as AutoMobileApp).productRepository)
+    }
+
+    private var cartItems = mutableListOf<Cart>()
+    private val totalCartPrice= MutableLiveData<Int>()
+    private var quantity: Int = 0
+    private var amt: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +60,40 @@ class PaymentFragment : Fragment() {
             dialog.setContentView(view)
             dialog.show()
         }
+
+        val cartView = binding.categoriesRecView
+        cartView.adapter = recyclerAdapter
+        cartView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        cartView.setHasFixedSize(true)
+
+         productViewmodel.getAllFromCart.observe(viewLifecycleOwner, Observer {list->
+            //recyclerAdapter.submitList(it)
+              list?.let {
+                recyclerAdapter.submitList(it)
+                cartItems.clear()
+                amt =0
+                cartItems.addAll(it)
+            }
+
+            cartItems.forEach {
+                  amt += it.price!!.toInt()
+                 quantity += it.quantity
+                 totalCartPrice.value = amt
+                Log.d("AMOUNT", ":::::::::::::::::::${amt}")
+            }
+             binding.itemCount.text = "Items $quantity"
+        })
+
+        totalCartPrice.observe(viewLifecycleOwner, Observer {expenses->
+                Log.d("Total", "OBSERVER:::::::::::::::::::${expenses}")
+                binding.order.text = "*Ghc $expenses"
+            binding.totalPriceFrag.text = "*Ghc ${expenses+30}"
+            })
+
+
+
+
+
         return binding.root
     }
 
@@ -66,6 +123,10 @@ class PaymentFragment : Fragment() {
                     }
             }
         }
+    }
+
+    override fun onItemDeleteListener(car: Cart) {
+        productViewmodel.deleteSingleCart(car)
     }
 
 }
